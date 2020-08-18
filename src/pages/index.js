c8e4c0a9cdc32f714685aa3dfdc88c4599a0fa19
TuanTQ <tquoctua97@gmail.com>
2020-08-18 13:47:53 +0700

    add query to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import React from "react"
 import MainLayout from "../layouts/MainLayout"
 import Post from "../components/Post"
+import { graphql } from "gatsby"
 
-const Home = () => (
-  <MainLayout className="justify-content-center">
-    <Post title="Hello World" excerpt="This is a exceprt" />
-    <Post title="Hello World" excerpt="This is a exceprt" />
-    <Post title="Hello World" excerpt="This is a exceprt" />
-    <Post title="Hello World" excerpt="This is a exceprt" />
-  </MainLayout>
-)
+const Home = ({ data }) => {
+  console.log(data)
+  return (
+    <MainLayout className="justify-content-center">
+      <Post title="Hello World" excerpt="This is a exceprt" />
+      <Post title="Hello World" excerpt="This is a exceprt" />
+      <Post title="Hello World" excerpt="This is a exceprt" />
+      <Post title="Hello World" excerpt="This is a exceprt" />
+    </MainLayout>
+  )
+}
 
 export default Home
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          date
+          keywords
+          image
+        }
+        html
+        excerpt
+      }
+    }
+  }
+`
