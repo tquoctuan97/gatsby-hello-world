98b5ea730b33d329ec1f9e76faad26566184a2bd
TuanTQ <tquoctua97@gmail.com>
2020-08-19 02:03:48 +0700

    git config core.ignorecase false

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "gatsby"
+
+export default props => (
+  <Navbar bg="light" expand="md">
+    <Navbar.Brand>
+      <Link to="/">Gatsby-Bootstrap</Link>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/">
+          Home
+        </Nav.Link>
+        <Nav.Link as={Link} to="/about">
+          About Us
+        </Nav.Link>
+        <Nav.Link as={Link} to="/contact">
+          Contact Us
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
