6c996403e8c9e6b873f68c207578d8349921f6ef
TuanTQ <tquoctua97@gmail.com>
2020-08-19 01:53:35 +0700

    gatsby build

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,5 @@
 import React from "react"
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "gatsby"
 
 export default props => (
